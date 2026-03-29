aa7888df98439e3275ece0cbebfb5c60d3a53c9f
vvlaads <vvlaads@gmail.com>
2026-03-30 02:34:50 +0300

    Updated main.py: added comments, added callings for triangle and circle distributions

--- src/main.py
+++ src/main.py
@@ -1,1 +1,40 @@
-print("Hello World")
+import random
+
+from methods import *
+from vector import Vector
+
+
+def print_sep():
+    """Печать разделителя для лучшей читаемости вывода"""
+    print("=" * 100)
+
+
+random.seed(42)  # Сид для генерации случайных чисел
+
+# Распределение для треугольника
+v1 = Vector(0, 0, 0)
+v2 = Vector(1, 1, 0)
+v3 = Vector(1, 0, 0)
+triangle_dots = triangle_distribution(random, v1, v2, v3, 100_000)
+print("Распределение для треугольника")
+print("Первые 10 точек:")
+for i in range(10):
+    print(triangle_dots[i])
+
+print_sep()
+
+# Распределение для круга
+n = Vector(0, 0, 1)
+center = Vector(0, 0, 0)
+radius = 5
+circle_dots = circle_distribution(random, n, radius, center, 100)
+print("Распределение для круга")
+print("Первые 10 точек:")
+for i in range(10):
+    print(circle_dots[i])
+
+print_sep()
+
+# TODO: Распределение для сферы
+
+# TODO: Косинусное распределение
